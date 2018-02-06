0e98a1ab11cd558e959ed51563c483c49ac6dfbf
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-02-05 20:46:09 -0600

    Loop through all tesserae files

--- feature.py
+++ feature.py
@@ -1,13 +1,29 @@
+import os
+
+greek_text_dir = "tesserae/texts/grc"
+
 def main():
 	text_to_features = {}
-	with open("tesserae/texts/grc/achilles_tatius.leucippe_et_clitophon.tess", "r") as file:
-		num_interrogative = 0
-		num_regular_sentence = 0
-		for line in file:
-			line = line[line.index('>') + 1:] #parse out .tess tags
-			num_interrogative += line.count(';')
-			num_regular_sentence += line.count('.')
-		print("Fraction of Interrogatives: " + str(num_interrogative / (num_interrogative + num_regular_sentence)))
+	file_names = []
+	for current_path, current_dir_names, current_file_names in os.walk(greek_text_dir):
+		file_names += [current_path + os.sep + current_file_name for current_file_name in current_file_names]
+
+	for file_name in file_names:
+		print("Reading " + file_name)
+		with open(file_name, "r") as file:
+			num_interrogative = 0
+			num_regular_sentence = 0
+			for line in file:
+
+				#Ignore lines without tess tags, or parse the tags out if they exist
+				if not line.startswith('<'):
+					continue
+				line = line[line.index('>') + 1:].strip()
+
+				num_interrogative += line.count(';')
+				num_regular_sentence += line.count('.')
+
+			print("	Fraction of Interrogatives: " + str(num_interrogative / (num_interrogative + num_regular_sentence)))
 
 if __name__ == "__main__":
 	main()
