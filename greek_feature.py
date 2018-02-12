4f0955ee7f3f195295cc17e54ed7fb5b7b92b3dc
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-02-11 19:58:53 -0600

    Conditional characters, and pre-process tess files

--- greek_feature.py
+++ greek_feature.py
@@ -1,28 +1,30 @@
 import os
-
-tesserae_clone_command = "git clone https://github.com/tesserae/tesserae.git"
-greek_text_dir = "tesserae/texts/grc"
+import sys
 
 def feature_interrogative(file):
 	num_interrogative = 0
 	num_regular_sentence = 0
+
 	for line in file:
-
-		#Ignore lines without tess tags, or parse the tag out and strip whitespace
-		if not line.startswith("<"):
-			continue
-		assert ">" in line
-		line = line[line.index(">") + 1:].strip()
-
 		num_interrogative += line.count(";")
 		num_regular_sentence += line.count(".")
 
 	return num_interrogative / (num_interrogative + num_regular_sentence)
 
-def feature_freq_conditional_clause(file):
-	return 0
+def feature_freq_conditional_characters(file):
+	num_conditional_characters = 0
+	num_characters = 0
 
-feature_list = [feature_interrogative, feature_freq_conditional_clause]
+	for line in file:
+		num_conditional_characters += line.count("εἲ") + line.count("ἐάν")
+		num_characters += len(line)
+
+	return num_conditional_characters / num_characters
+
+feature_list = [feature_interrogative, feature_freq_conditional_characters]
+
+tesserae_clone_command = "git clone https://github.com/tesserae/tesserae.git"
+greek_text_dir = "tesserae/texts/grc"
 
 def main():
 
@@ -35,17 +37,27 @@ def main():
 	text_to_features = {}
 	file_names = []
 	for current_path, current_dir_names, current_file_names in os.walk(greek_text_dir):
-		file_names += [current_path + os.sep + current_file_name for current_file_name in current_file_names]
+		for current_file_name in current_file_names:
+			f = str(current_path + os.sep + current_file_name)
+			file_names.append(f)
+			text_to_features[f] = {}
 
 	#Feature extraction
 	for file_name in file_names:
-		print("Reading " + file_name)
-		text_to_features[file_name] = {}
+		file_text = []
 		with open(file_name, "r") as file:
-			for feature in feature_list:
-				score = feature(file)
-				text_to_features[file_name][feature] = score
-				print("\t" + file_name + ", " + str(feature) + ", " + str(score))
+			for line in file:
+				#Ignore lines without tess tags, or parse the tag out and strip whitespace
+				if not line.startswith("<"):
+					continue
+				assert ">" in line
+				line = line[line.index(">") + 1:].strip()
+				file_text.append(line)
+
+		for feature in feature_list:
+			score = feature(file_text)
+			text_to_features[file_name][feature] = score
+			print(file_name + ", " + str(feature) + ", " + str(score))
 
 if __name__ == "__main__":
 	main()
