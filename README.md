f74987ff467568bb0e7fd72cdde44c21d98281b0
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-02-13 01:54:24 -0600

    Use cltk, update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 Parsing Ancient Greek texts to extract and quantify features.
 
+Python 3.6.0
+
+Dependencies:
+pip install cltk
+
+In order to use cltk utilities, cltk must download a Github repository onto your filesystem at ~/cltk\_data/greek/model/greek\_models\_cltk
+The repository is from https://github.com/cltk/greek\_models\_cltk.git
+
+To have cltk auto-install everything it needs, run the following commands in the python interpreter. The last command may take a few moments to complete.
+
+from cltk.corpus.utils.importer import CorpusImporter
+corpus\_importer = CorpusImporter('greek')
+corpus\_importer.import\_corpus('greek\_models\_cltk')
+
--- greek_feature.py
+++ greek_feature.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from cltk.tokenize.sentence import TokenizeSentence
 
 def feature_interrogative(file):
 	num_interrogative = 0
@@ -35,8 +36,7 @@ def main():
 	file_names = None
 	if len(sys.argv) > 1:
 		if sys.argv[1] == "debug": #if debug, just scan pre-selected corpus
-			file_names = ["tesserae/texts/grc/polybius.histories.tess", "tesserae/texts/grc/flavius_josephus.antiquitates_judaicae.tess", \
-			"tesserae/texts/grc/dionysius_halicarnassensis.antiquitates_romanae.tess"]
+			file_names = ["tesserae/texts/grc/polybius.histories.tess"]
 		else: #Allows user to select custom path other than tesserae
 			greek_text_dir = sys.argv[1]
 
@@ -52,8 +52,10 @@ def main():
 
 	#Feature extraction
 	for file_name in file_names:
-		file_text = []
 		text_to_features[file_name] = {}
+
+		#Store each line of file in a list
+		file_text = []
 		with open(file_name, "r") as file:
 			for line in file:
 				#Ignore lines without tess tags, or parse the tag out and strip whitespace
@@ -63,6 +65,12 @@ def main():
 				line = line[line.index(">") + 1:].strip()
 				file_text.append(line)
 
+		#Convert list of strings into a single string, then break that into sentences using cltk tokenizer
+		file_text = " ".join(file_text)
+		tokenizer = TokenizeSentence("greek")
+		file_text = tokenizer.tokenize_sentences(file_text)
+
+		#Iterate over features
 		for feature in feature_list:
 			score = feature(file_text)
 			text_to_features[file_name][feature] = score
@@ -70,3 +78,25 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+"""
+File "greek_feature.py", line 81, in <module>
+    main()
+  File "greek_feature.py", line 71, in main
+    tokenizer = TokenizeSentence("greek")
+  File "/Users/timgianitsos/Git/FeatureExtraction/env/lib/python3.6/site-packages/cltk/tokenize/sentence.py", line 35, in __init__
+    self._setup_language_variables(self.language)
+  File "/Users/timgianitsos/Git/FeatureExtraction/env/lib/python3.6/site-packages/cltk/tokenize/sentence.py", line 56, in _setup_language_variables
+    'CLTK linguistics data not found for language {0}'.format(lang)
+AssertionError: CLTK linguistics data not found for language greek
+
+file = PUNCTUATION[lang]['file']
+ 11         rel_path = os.path.join('~/cltk_data',
+ 10                                 lang,
+  9                                 'model/' + lang + '_models_cltk/tokenizers/sentence')  # pylint: disable=C0301
+  8         path = os.path.expanduser(rel_path)
+  7         tokenizer_path = os.path.join(path, file)
+  6         assert os.path.isfile(tokenizer_path), \
+  5             'CLTK linguistics data not found for language {0}'.format(lang)
+
+"""
