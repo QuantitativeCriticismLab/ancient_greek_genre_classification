5fb9a22aaa1e8ddab51c0eb75d00ec6072b46208
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-06-04 01:56:02 -0500

    Parameterize decorator language

--- decorator.py
+++ decorator.py
@@ -1,35 +1,23 @@
 from cltk.tokenize.sentence import TokenizeSentence
 from cltk.tokenize.word import WordTokenizer
 
+last_tokenized_sentence_filename = None
 sentence_tokens = None #cached tokenized file
 
-def tokenize_sentences(f):
-	tokenizer = TokenizeSentence('greek') #TODO other languages
-	def tokenized_input_sen(file):
-		eq = True
-		global sentence_tokens
+def tokenize_sentences(lang):
+	def decor(f):
+		def wrapper(file, filename):
+			global sentence_tokens
+			global last_tokenized_sentence_filename
+			if not last_tokenized_sentence_filename or last_tokenized_sentence_filename != filename:
+				tokenizer = TokenizeSentence(lang)
+				last_tokenized_sentence_filename = filename
+				sentence_tokens = tokenizer.tokenize_sentences(file)
+			return f(sentence_tokens)
+		return wrapper
+	return decor
 
-		#Check if the cached tokenized file is equal to the provided file
-		if sentence_tokens is None:
-			eq = False
-		else:
-			i = 0
-			while i < len(file) and eq:
-				for tok in sentence_tokens:
-					if tok != file[i: len(tok)]:
-						eq = False
-						break
-					i += len(tok)
-
-
-		if not eq:
-			sentence_tokens = tokenizer.tokenize_sentences(file)
-		else:
-			print('Cache hit: ' + str(f))
-		return f(sentence_tokens)
-	return tokenized_input_sen
-
-@tokenize_sentences
+@tokenize_sentences('greek')
 def freq_interrogatives(file):
 	num_interrogative = 0
 
@@ -38,7 +26,7 @@ def freq_interrogatives(file):
 
 	return num_interrogative / len(file)
 
-@tokenize_sentences
+@tokenize_sentences('greek')
 def bar(file):
 	return 0
 
@@ -47,6 +35,7 @@ def freq_conditional_characters(file):
 	file = WordTokenizer('greek').tokenize(file)
 	num_conditional_characters = 0
 	num_characters = 0
+
 	conditional_characters = {'εἰ', 'εἴ', 'εἲ', 'ἐάν', 'ἐὰν'}
 	conditional_characters = conditional_characters | \
 	{normalize('NFD', val) for val in conditional_characters} | \
@@ -62,23 +51,49 @@ def freq_conditional_characters(file):
 
 #Tests
 
+# file = 'a' * 100
+
+# freq_interrogatives(file)
+# freq_interrogatives(file)
+# bar(file)
+# freq_interrogatives(file)
+# bar(file)
+# bar(file)
+
+# print()
+
+# file = 'a' * 99
+# bar(file)
+# freq_interrogatives(file)
+
+# print()
+
+# file = 'a' * 101
+# freq_interrogatives(file)
+# bar(file)
+
+#Tests 2
+
 file = 'a' * 100
+filename = 'abc/def'
 
-freq_interrogatives(file)
-freq_interrogatives(file)
-bar(file)
-freq_interrogatives(file)
-bar(file)
-bar(file)
-
-print()
-
-file = 'a' * 99
-bar(file)
-freq_interrogatives(file)
+freq_interrogatives(file, filename)
+freq_interrogatives(file, filename)
+bar(file, filename)
+freq_interrogatives(file, filename)
+bar(file, filename)
+bar(file, filename)
 
 print()
 
+filename = 'abc/ghi'
+bar(file, filename)
+freq_interrogatives(file, filename)
+
+print()
+
+filename = 'abc/jkl'
 file = 'a' * 101
-freq_interrogatives(file)
-bar(file)
+freq_interrogatives(file, filename)
+bar(file, filename)
+
