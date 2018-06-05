a6efe932613062b1b484f4c39120ab01bcda7ba1
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-06-05 12:44:33 -0500

    Iterate over decorated features

--- decorator.py
+++ decorator.py
@@ -1,8 +1,7 @@
 from cltk.tokenize.sentence import TokenizeSentence
 from cltk.tokenize.word import WordTokenizer
-from unicodedata import normalize
 
-features = []
+decorated_features = []
 
 tokenize_types = {\
 	'sentences': {\
@@ -17,97 +16,16 @@ tokenize_types = {\
 	}
 }
 
-def textual_feature(tokenize_type, lang):
+#TODO think about if decorator parameter is None?
+def textual_feature(tokenize_type, lang, debug=False):
 	def decor(f):
 		def wrapper(file, filename):
-			global tokenize_types
 			if tokenize_types[tokenize_type]['prev_filename'] != filename:
 				tokenize_types[tokenize_type]['prev_filename'] = filename
 				tokenize_types[tokenize_type]['tokens'] = tokenize_types[tokenize_type]['func'](lang, file)
-			else:
-				print('Cache hit: ' + f.__name__ + ' ' + filename)
+			elif debug:
+				print('*** Cache hit! ' + 'function: <' + f.__name__ + '>, filename: ' + filename + ' ***')
 			return f(tokenize_types[tokenize_type]['tokens'])
-		features.append(f.__name__)
+		decorated_features.append((wrapper, f.__name__))
 		return wrapper
 	return decor
-
-@textual_feature('sentences', 'greek')
-def freq_interrogatives(file):
-	num_interrogative = 0
-
-	for line in file:
-		num_interrogative += line.count(';') + line.count(';')
-
-	return num_interrogative / len(file)
-
-@textual_feature('sentences', 'greek')
-def bar(file):
-	return 0
-
-@textual_feature('sentences', 'greek')
-def taz(file):
-	return 0
-
-@textual_feature('words', 'greek')
-def freq_conditional_characters(file):
-	num_conditional_characters = 0
-	num_characters = 0
-
-	conditional_characters = {'εἰ', 'εἴ', 'εἲ', 'ἐάν', 'ἐὰν'}
-	conditional_characters = conditional_characters | \
-	{normalize('NFD', val) for val in conditional_characters} | \
-	{normalize('NFC', val) for val in conditional_characters} | \
-	{normalize('NFKD', val) for val in conditional_characters} | \
-	{normalize('NFKC', val) for val in conditional_characters}
-
-	for word in file:
-		num_conditional_characters += len(word) if word in conditional_characters else 0
-		num_characters += len(word)
-
-	return num_conditional_characters / num_characters
-
-@textual_feature('sentences', 'greek')
-def qux(file):
-	return 0
-
-@textual_feature('sentences', 'greek')
-def lup(file):
-	return 0
-
-#Tests
-
-file = 'a' * 100
-filename = 'abc/def'
-
-freq_interrogatives(file, filename)
-freq_interrogatives(file, filename)
-bar(file, filename)
-freq_interrogatives(file, filename)
-bar(file, filename)
-bar(file, filename)
-
-print()
-
-filename = 'abc/ghi'
-bar(file, filename)
-freq_interrogatives(file, filename)
-
-print()
-
-filename = 'abc/jkl'
-file = 'a' * 101
-freq_interrogatives(file, filename)
-bar(file, filename)
-
-print()
-
-freq_conditional_characters(file, filename)
-freq_conditional_characters(file, filename)
-
-print()
-
-print('Iteration:')
-filename = 'abc/mno'
-for f in features:
-	print('\t' + f)
-	globals()[f](file, filename)
---
+++ test_decorator.py
@@ -0,0 +1,113 @@
+from decorator import textual_feature, decorated_features, tokenize_types
+
+@textual_feature('sentences', 'greek', debug=True)
+def foo(file):
+	return 'I am foo\n' + str(file) + '\n\n'
+
+@textual_feature('sentences', 'greek', debug=True)
+def bar(file):
+	return 'I am bar\n' + str(file) + '\n\n'
+
+@textual_feature('words', 'greek', debug=True)
+def taz(file):
+	return 'I am taz\n' + str(file) + '\n\n'
+
+@textual_feature('words', 'greek', debug=True)
+def qux(file):
+	return 'I am qux\n' + str(file) + '\n\n'
+
+@textual_feature('sentences', 'greek', debug=True)
+def rup(file):
+	return 'I am rup\n' + str(file) + '\n\n'
+
+@textual_feature('sentences', 'greek', debug=True)
+def lon(file):
+	return 'I am lon\n' + str(file) + '\n\n'
+
+file = 'test test. test test test test test test; test test. test.'
+filename = 'abc/def'
+
+for t in decorated_features:
+	print(t[0](file, filename))
+
+# from unicodedata import normalize
+# @textual_feature('sentences', 'greek')
+# def freq_interrogatives(file):
+# 	num_interrogative = 0
+
+# 	for line in file:
+# 		num_interrogative += line.count(';') + line.count(';')
+
+# 	return num_interrogative / len(file)
+
+# @textual_feature('sentences', 'greek')
+# def bar(file):
+# 	return 0
+
+# @textual_feature('sentences', 'greek')
+# def taz(file):
+# 	return 0
+
+# @textual_feature('words', 'greek')
+# def freq_conditional_characters(file):
+# 	num_conditional_characters = 0
+# 	num_characters = 0
+
+# 	conditional_characters = {'εἰ', 'εἴ', 'εἲ', 'ἐάν', 'ἐὰν'}
+# 	conditional_characters = conditional_characters | \
+# 	{normalize('NFD', val) for val in conditional_characters} | \
+# 	{normalize('NFC', val) for val in conditional_characters} | \
+# 	{normalize('NFKD', val) for val in conditional_characters} | \
+# 	{normalize('NFKC', val) for val in conditional_characters}
+
+# 	for word in file:
+# 		num_conditional_characters += len(word) if word in conditional_characters else 0
+# 		num_characters += len(word)
+
+# 	return num_conditional_characters / num_characters
+
+# @textual_feature('sentences', 'greek')
+# def qux(file):
+# 	return 0
+
+# @textual_feature('sentences', 'greek')
+# def lup(file):
+# 	return 0
+
+# #Tests
+
+# file = 'a' * 100
+# filename = 'abc/def'
+
+# freq_interrogatives(file, filename)
+# freq_interrogatives(file, filename)
+# bar(file, filename)
+# freq_interrogatives(file, filename)
+# bar(file, filename)
+# bar(file, filename)
+
+# print()
+
+# filename = 'abc/ghi'
+# bar(file, filename)
+# freq_interrogatives(file, filename)
+
+# print()
+
+# filename = 'abc/jkl'
+# file = 'a' * 101
+# freq_interrogatives(file, filename)
+# bar(file, filename)
+
+# print()
+
+# freq_conditional_characters(file, filename)
+# freq_conditional_characters(file, filename)
+
+# print()
+
+# print('Iteration:')
+# filename = 'abc/mno'
+# for f in features:
+# 	print('\t' + f)
+# 	globals()[f](file, filename)
