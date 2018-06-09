bf9f185183b296411927a7c8c02492cbfd634ce2
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-06-09 00:49:54 -0500

    Decorator unit tests

--- test_decorator.py
+++ test_decorator.py
@@ -1,113 +1,86 @@
-from textual_feature import textual_feature, decorated_features, tokenize_types
+from textual_feature import textual_feature, decorated_features, tokenize_types, clear_cache, debug_output
+import unittest
 
 @textual_feature('sentences', 'greek', debug=True)
 def foo(file):
-	return 'I am foo\n' + str(file) + '\n\n'
+	return 'foo'
 
 @textual_feature('sentences', 'greek', debug=True)
 def bar(file):
-	return 'I am bar\n' + str(file) + '\n\n'
+	return 'bar'
 
 @textual_feature('words', 'greek', debug=True)
 def taz(file):
-	return 'I am taz\n' + str(file) + '\n\n'
+	return 'taz'
 
 @textual_feature('words', 'greek', debug=True)
 def qux(file):
-	return 'I am qux\n' + str(file) + '\n\n'
+	return 'qux'
 
 @textual_feature('sentences', 'greek', debug=True)
 def rup(file):
-	return 'I am rup\n' + str(file) + '\n\n'
+	return 'rup'
 
 @textual_feature('sentences', 'greek', debug=True)
 def lon(file):
-	return 'I am lon\n' + str(file) + '\n\n'
+	return 'lon'
 
-file = 'test test. test test test test test test; test test. test.'
-filename = 'abc/def'
+@textual_feature('sentences', 'greek', debug=True)
+def return_sentences(file):
+	return file
 
-for t in decorated_features:
-	print(t[0](file, filename))
+@textual_feature('words', 'greek', debug=True)
+def return_words(file):
+	return file
 
-# from unicodedata import normalize
-# @textual_feature('sentences', 'greek')
-# def freq_interrogatives(file):
-# 	num_interrogative = 0
+class TestTextualFeature(unittest.TestCase):
+	def test_cache(self):
+		file = 'test test. test test test test test test; test test. test.'
 
-# 	for line in file:
-# 		num_interrogative += line.count(';') + line.count(';')
+		filename = 'abc/def'
+		foo(file, filename)
+		bar(file, filename)
+		rup(file, filename)
+		self.assertEqual(debug_output.getvalue(), 'Cache hit! function: <bar>, filename: abc/def\n' + \
+			'Cache hit! function: <rup>, filename: abc/def\n')
 
-# 	return num_interrogative / len(file)
+		clear_cache(tokenize_types, debug_output)
 
-# @textual_feature('sentences', 'greek')
-# def bar(file):
-# 	return 0
+		filename = 'abc/ghi'
+		foo(file, filename)
+		taz(file, filename)
+		qux(file, filename)
+		self.assertEqual(debug_output.getvalue(), 'Cache hit! function: <qux>, filename: abc/ghi\n')
+		filename = 'abc/jkl'
+		foo(file, filename)
+		bar(file, filename)
+		taz(file, filename)
+		qux(file, filename)
+		self.assertEqual(debug_output.getvalue(), 'Cache hit! function: <qux>, filename: abc/ghi\n' + \
+			'Cache hit! function: <bar>, filename: abc/jkl\n' + \
+			'Cache hit! function: <qux>, filename: abc/jkl\n')
 
-# @textual_feature('sentences', 'greek')
-# def taz(file):
-# 	return 0
+	def test_sentence_tokenization(self):
+		file = 'test test. test test test test test test; test test. test.'
+		filename = 'abc/def'
+		self.assertEqual(return_sentences(file, filename), ['test test.', 'test test test test test test;', \
+			'test test.', 'test.'])
 
-# @textual_feature('words', 'greek')
-# def freq_conditional_characters(file):
-# 	num_conditional_characters = 0
-# 	num_characters = 0
+	def test_word_tokenization(self):
+		file = 'test test. test test test test test test; test test. test.'
+		filename = 'abc/def'
+		self.assertEqual(return_words(file, filename), ['test', 'test', '.', 'test', 'test', 'test', 'test', \
+			'test', 'test', ';', 'test', 'test', '.', 'test', '.'])
 
-# 	conditional_characters = {'εἰ', 'εἴ', 'εἲ', 'ἐάν', 'ἐὰν'}
-# 	conditional_characters = conditional_characters | \
-# 	{normalize('NFD', val) for val in conditional_characters} | \
-# 	{normalize('NFC', val) for val in conditional_characters} | \
-# 	{normalize('NFKD', val) for val in conditional_characters} | \
-# 	{normalize('NFKC', val) for val in conditional_characters}
+	def test_loop_over_all_features(self):
+		clear_cache(tokenize_types, debug_output)
+		file = 'test test. test test test test test test; test test. test.'
+		filename = 'abc/def'
+		outputs = ['foo', 'bar', 'taz', 'qux', 'rup', 'lon', ['test test.', 'test test test test test test;', \
+			'test test.', 'test.'], ['test', 'test', '.', 'test', 'test', 'test', 'test', 'test', 'test', ';', 'test', \
+			'test', '.', 'test', '.']]
+		for i in range(len(decorated_features)):
+			self.assertEqual(decorated_features[i][0](file, filename), outputs[i])
 
-# 	for word in file:
-# 		num_conditional_characters += len(word) if word in conditional_characters else 0
-# 		num_characters += len(word)
-
-# 	return num_conditional_characters / num_characters
-
-# @textual_feature('sentences', 'greek')
-# def qux(file):
-# 	return 0
-
-# @textual_feature('sentences', 'greek')
-# def lup(file):
-# 	return 0
-
-# #Tests
-
-# file = 'a' * 100
-# filename = 'abc/def'
-
-# freq_interrogatives(file, filename)
-# freq_interrogatives(file, filename)
-# bar(file, filename)
-# freq_interrogatives(file, filename)
-# bar(file, filename)
-# bar(file, filename)
-
-# print()
-
-# filename = 'abc/ghi'
-# bar(file, filename)
-# freq_interrogatives(file, filename)
-
-# print()
-
-# filename = 'abc/jkl'
-# file = 'a' * 101
-# freq_interrogatives(file, filename)
-# bar(file, filename)
-
-# print()
-
-# freq_conditional_characters(file, filename)
-# freq_conditional_characters(file, filename)
-
-# print()
-
-# print('Iteration:')
-# filename = 'abc/mno'
-# for f in features:
-# 	print('\t' + f)
-# 	globals()[f](file, filename)
+if __name__ == '__main__':
+	unittest.main()
--- textual_feature.py
+++ textual_feature.py
@@ -1,9 +1,15 @@
 from cltk.tokenize.sentence import TokenizeSentence
 from cltk.tokenize.word import WordTokenizer
+from io import StringIO
 
 decorated_features = []
 
 tokenize_types = {\
+	'default': {\
+		'func': lambda lang, file: file, \
+		'prev_filename': None, \
+		'tokens': None, \
+	}, \
 	'sentences': {\
 		'func': lambda lang, file: TokenizeSentence(lang).tokenize_sentences(file), \
 		'prev_filename': None, \
@@ -13,18 +19,27 @@ tokenize_types = {\
 		'func': lambda lang, file: WordTokenizer(lang).tokenize(file), \
 		'prev_filename': None, \
 		'tokens': None, \
-	}
+	}, \
 }
 
-#TODO think about if decorator parameter is None?
+debug_output = StringIO()
+
+def clear_cache(cache, debug):
+	for k, v in cache.items():
+		v['prev_filename'] = None
+		v['tokens'] = None
+	debug.truncate(0)
+	debug.seek(0)
+
 def textual_feature(tokenize_type, lang, debug=False):
+	assert tokenize_type in tokenize_types, '"' + str(tokenize_type) + '" is not a valid tokenize type'
 	def decor(f):
 		def wrapper(file, filename):
 			if tokenize_types[tokenize_type]['prev_filename'] != filename:
 				tokenize_types[tokenize_type]['prev_filename'] = filename
 				tokenize_types[tokenize_type]['tokens'] = tokenize_types[tokenize_type]['func'](lang, file)
 			elif debug:
-				print('*** Cache hit! ' + 'function: <' + f.__name__ + '>, filename: ' + filename + ' ***')
+				debug_output.write('Cache hit! ' + 'function: <' + f.__name__ + '>, filename: ' + filename + '\n')
 			return f(tokenize_types[tokenize_type]['tokens'])
 		decorated_features.append((wrapper, f.__name__))
 		return wrapper
