24d3a15dc6c9e1a4fa1c2dd852c8c139a0fd169b
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-06-10 03:51:36 -0500

    Split greek file into features and runner

--- greek_feature.py
+++ greek_feature.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import math
-import pickle
-from textual_feature import textual_feature, decorated_features
-from color import RED, GREEN, RESET
+import extract_features
+from textual_feature import textual_feature
 from functools import reduce
-from cltk.tokenize.sentence import TokenizeSentence
 from cltk.tokenize.word import WordTokenizer
 from unicodedata import normalize #the cltk_normalize cannot decompose (only has NFC & NFKC, not NFD or NFKD)
 #Reference: https://jktauber.com/articles/python-unicode-ancient-greek/
@@ -548,53 +543,5 @@ def freq_men(file):
 		num_characters += 1
 	return num_men / num_characters
 
-
-tesserae_clone_command = "git clone https://github.com/tesserae/tesserae.git"
-greek_text_dir = "tesserae/texts/grc"
-
-def main():
-	text_to_features = {} #Associates file names to their respective features
-	file_names = None
-
-	#Download corpus if non-existent
-	if not os.path.isdir(greek_text_dir):
-		print(RED + "Corpus at " + greek_text_dir + " does not exist - attempting to clone repository..." + RESET)
-		os.system(tesserae_clone_command)
-
-	#Obtain all the files to parse by traversing through the directory
-	file_names = {current_path + os.sep + current_file_name for current_path, current_dir_names, current_file_names in \
-	os.walk(greek_text_dir) for current_file_name in current_file_names if current_file_name.endswith(".tess")}
-
-	#Feature extraction
-	for file_name in file_names:
-		text_to_features[file_name] = {}
-
-		#Store each line of file in a list
-		file_text = []
-		with open(file_name, mode="r", encoding="utf-8") as file:
-			for line in file:
-				#Ignore lines without tess tags, or parse the tag out and strip whitespace
-				if not line.startswith("<"):
-					continue
-				assert ">" in line
-				line = line[line.index(">") + 1:].strip()
-				file_text.append(line)
-
-		#Convert list of strings into a single string
-		file_text = " ".join(file_text)
-
-		#Default behavior is to invoke ALL decorated functions. If names of features are specified on the 
-		#command line, then only invoke those
-		for feature_name, func in decorated_features.items() if len(sys.argv) == 1 else \
-		[(sys.argv[i], decorated_features[sys.argv[i]]) for i in range(1, len(sys.argv)) if sys.argv[i] in decorated_features]:
-			score = func(file_text, file_name)
-			text_to_features[file_name][feature_name] = score
-			print(file_name + ", " + str(feature_name) + ", " + GREEN + str(score) + RESET)
-
-	# with open("matrix.pickle", "wb") as pickle_file:
-	# 	pickle_file.write(pickle.dumps(text_to_features))
-	# 	pickle_file.close()
-
-if __name__ == "__main__":
-	from timeit import timeit
-	print('\n\n' + GREEN + 'Elapsed time: ' + str(timeit(main, number=1)) + RESET)
+if __name__ == '__main__':
+	extract_features.main()
