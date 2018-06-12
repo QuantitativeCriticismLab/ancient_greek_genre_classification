42212968054ea1f847c9cf2352ce2a90d7980778
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-06-12 08:21:20 -0500

    Use StringIO for file parsing

--- extract_features.py
+++ extract_features.py
@@ -4,16 +4,17 @@ from color import RED, GREEN, RESET
 from textual_feature import decorated_features
 
 def parse_tess(file_name):
-	file_text = []
+	from io import StringIO
+	file_text = StringIO()
 	with open(file_name, mode='r', encoding='utf-8') as file:
 		for line in file:
 			#Ignore lines without tess tags, or parse the tag out and strip whitespace
 			if not line.startswith('<'):
 				continue
 			assert '>' in line
-			line = line[line.index('>') + 1:].strip()
-			file_text.append(line)
-	return ' '.join(file_text)
+			file_text.write(line[line.index('>') + 1:].strip())
+			file_text.write(' ')
+	return file_text.getvalue()
 
 file_parsers = {\
 	'tess': parse_tess, \
@@ -26,7 +27,6 @@ def __extract_features(corpus_dir, file_extension, features):
 	#Obtain all the files to parse by traversing through the directory
 	file_names = sorted(list({current_path + os.sep + current_file_name for current_path, current_dir_names, current_file_names in \
 	os.walk(corpus_dir) for current_file_name in current_file_names if current_file_name.endswith('.' + file_extension)}))
-
 	feature_tuples = [(name, decorated_features[name]) for name in features]
 
 	#Feature extraction
