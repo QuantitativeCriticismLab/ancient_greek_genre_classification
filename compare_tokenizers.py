fcea99ada60827c4532d3fefa1193dcc6ec61c4f
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2018-07-10 03:49:09 +0300

    Comparing tokenizers

---
+++ compare_tokenizers.py
@@ -0,0 +1,27 @@
+import os
+from cltk.utils.file_operations import open_pickle
+from extract_features import file_parsers
+from progress_bar import print_progress_bar
+
+xeno_tokenizer = open_pickle('tokenizers/ancient_greek.pickle')
+tess_tokenizer = open_pickle('notes/tesserae_greek.pickle')
+corpus_dir = 'tesserae' + os.sep + 'texts' + os.sep + 'grc'
+file_extension = 'tess'
+#Obtain all the files to parse by traversing through the directory
+file_names = sorted(list({current_path + os.sep + current_file_name for current_path, current_dir_names, current_file_names in \
+os.walk(corpus_dir) for current_file_name in current_file_names if current_file_name.endswith('.' + file_extension)}))
+
+f = open('notes/diff_tokenizers_xeno_tess.txt', mode='w')
+f.write('Differences:\n\n')
+counter = 1
+for file_name in ['tesserae/texts/grc/achilles_tatius.leucippe_et_clitophon.tess']: #file_names:
+	file_text = file_parsers[file_extension](file_name)
+	x_tokens = xeno_tokenizer.tokenize(file_text)
+	t_tokens = tess_tokenizer.tokenize(file_text)
+	if t_tokens != x_tokens:
+		xeno_out = open('notes/xeno_token_achilles.txt', mode='w')
+		xeno_out.write('\n'.join(x_tokens))
+		tess_out = open('notes/tess_token_achilles.txt', mode='w')
+		tess_out.write('\n'.join(t_tokens))
+	# print_progress_bar(counter, len(file_names))
+	counter += 1
--- count_xenophon_tokens.py
+++ count_xenophon_tokens.py
@@ -1,5 +1,25 @@
 from extract_features import parse_tess
 from cltk.utils.file_operations import open_pickle
+from nltk.tokenize.punkt import PunktLanguageVars, PunktTrainer, PunktSentenceTokenizer
+
+PunktLanguageVars.sent_end_chars = ('.', ';', ';')
+PunktLanguageVars.internal_punctuation = (',', '·', ':')
+
 text = parse_tess('tesserae/texts/grc/xenophon.anabasis.tess')
 tokenizer = open_pickle('tokenizers/ancient_greek.pickle')
-print(len(tokenizer.tokenize(text)))
+print('Xenophon tokens: ' + str(len(tokenizer.tokenize(text))))
+print()
+
+trainer = PunktTrainer(lang_vars=PunktLanguageVars())
+trainer.INCLUDE_ALL_COLLOCS = True
+trainer.INCLUDE_ABBREV_COLLOCS = True
+trainer.train(text, verbose=True)
+
+new_tokenizer = PunktSentenceTokenizer(trainer.get_params())
+print('tokenizers equal? ' + str(tokenizer == new_tokenizer))
+print('tokenization equal? ' + str(tokenizer.tokenize(text) == new_tokenizer.tokenize(text)))
+
+old_tok_out = open('notes/old_tok.txt', mode='w')
+old_tok_out.write('\n'.join(tokenizer.tokenize(text)))
+new_tok_out = open('notes/new_tok.txt', mode='w')
+new_tok_out.write('\n'.join(new_tokenizer.tokenize(text)))
