116ab759730f9312abc012cfe6e0f24933d1bc47
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2019-12-17 18:09:21 -0800

    Directory for data

--- create_dataset.py
+++ create_dataset.py
@@ -8,7 +8,7 @@ import qcrit.extract_features
 
 from download_corpus import download_corpus
 
-feature_data_file = sys.argv[1] if len(sys.argv) >= 2 else input('Enter pickle file: ')
+feature_data_file = sys.argv[1] if len(sys.argv) >= 2 else input('Enter pickle file to generate or extract: ')
 
 if not os.path.isfile(feature_data_file):
 	corpus_path = ('tesserae', 'texts', 'grc')
@@ -34,13 +34,19 @@ filename_to_features = _get_features(feature_data_file)
 filename_to_classification, labels = _get_file_classifications(classification_data_file)
 
 #For every set of features, there should be a corresponding file classification
-assert len(filename_to_features.keys() - filename_to_classification.keys()) == 0, 'file_to_feature len: ' + str(len(filename_to_features.keys())) + ', filename_to_classification len: ' + str(len(filename_to_classification.keys()))
+assert len(filename_to_features.keys() - filename_to_classification.keys()) == 0, (
+	'file_to_feature len: ' + str(len(filename_to_features.keys()))
+	+ ', filename_to_classification len: ' + str(len(filename_to_classification.keys()))
+)
 
 #Convert features and classifications into sorted lists
 file_names = sorted(filename_to_features.keys())
-feature_names = sorted(list({feature for feature_to_val in filename_to_features.values() for feature in feature_to_val.keys()}))
+feature_names = sorted(
+	list({feature for feature_to_val in filename_to_features.values() for feature in feature_to_val.keys()}))
 
-data, target = _get_classifier_data(filename_to_features, filename_to_classification, file_names, feature_names)
+data, target = _get_classifier_data(
+	filename_to_features, filename_to_classification, file_names, feature_names
+)
 
 assert len(data) == len(target) == len(file_names)
 
@@ -49,20 +55,26 @@ code_hash = os.popen('git rev-parse HEAD').read().strip()
 corpus_repo = os.popen('git -C "./tesserae" remote get-url origin').read().strip()
 corpus_hash = os.popen('git -C "./tesserae" rev-parse HEAD').read().strip()
 
-prose_file = open('prose_data.csv', mode='w')
-prose_file.write('Ancient Greek Prose Data\n')
-verse_file = open('verse_data.csv', mode='w')
-verse_file.write('Ancient Greek Verse Data\n')
+prose_file = open(os.path.join('extracted_data', 'prose_data.csv'), mode='w')
+prose_file.write('Ancient Greek Prose Data')
+verse_file = open(os.path.join('extracted_data', 'verse_data.csv'), mode='w')
+verse_file.write('Ancient Greek Verse Data')
 for f in (prose_file, verse_file):
-	f.write(
-		f'Code Repo: {code_repo}'
-		f',Corpus: {corpus_repo}'
-		f',Code commit: {code_hash}'
-		f',Corpus commit: {corpus_hash}'
-		f',Project: https://www.qcrit.org'
-		f',Author: Tim Gianitsos'
-		f',Note: Frequencies are per-character\n'
+	first_line = (
+		f',Code Repo: {code_repo}',
+		f',Corpus: {corpus_repo}',
+		f',Code commit: {code_hash}',
+		f',Corpus commit: {corpus_hash}',
+		f',Project: https://www.qcrit.org',
+		f',Author: Tim Gianitsos',
+		f',Note: Frequencies are per-character',
 	)
+	#having blank cells allows first row to match the dimensions of subsequent rows
+	#this will cause it to display on Github better
+	num_buffer_cells = data.shape[1] - len(first_line)
+
+	f.write(''.join(first_line))
+	f.write(',' * num_buffer_cells + '\n')
 	f.write('file name,' + ','.join(feature_names) + '\n')
 
 for i in range(len(data)):
